666c2fe082ec46415b6015922c15e5d4966049ee
edo_eja <duyanhsadg@gmail.com>
2025-03-22 13:39:54 +0700

    first

--- App.js
+++ App.js
@@ -1,82 +1,11 @@
-// filepath: /c:/Users/PC/HomeWork/App.js
-import React, { useContext } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { AuthProvider, AuthContext } from './components/AuthContext';
-import SignInScreen from './components/screens/SignInScreen';
-import SignUpScreen from './components/screens/SignUpScreen';
-import ExplorerScreen from './components/screens/ExplorerScreen';
-import AccountScreen from './components/screens/AccountScreen';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator";
 
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function MainTabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Explorer') {
-            iconName = focused ? 'fast-food-outline' : 'fast-food-outline';
-          } else if (route.name === 'Account') {
-            iconName = focused ? 'person-circle' : 'person-circle';
-          }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      }}
-    >
-      <Tab.Screen name="Explorer" component={ExplorerScreen} options={{ headerTitle: 'Explorer' }} />
-      <Tab.Screen name="Account" component={AccountScreen} options={{ headerTitle: 'Account' }} />
-    </Tab.Navigator>
-  );
+export default function App() {
+    return (
+        <NavigationContainer>
+            <AppNavigator />
+        </NavigationContainer>
+    );
 }
-
-function App() {
-  const { isLoggedIn } = useContext(AuthContext);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={isLoggedIn ? "MainTabs" : "SignIn"}>
-        {!isLoggedIn ? (
-          <>
-            <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerTitle: 'Sign Up' }} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }} />
-          </>
-        )}
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
-  );
-}
-
-export default function Main() {
-  return (
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-Họ và tên: Vũ Nguyễn Duy Anh
-Mã sinh viên: 22810310266
+## Thông tin cá nhân
 
+**Họ và tên:** Vũ Nguyễn Duy Anh 
+**MSSV:** 22810310266
 
+![alt text](image-1.png)
 ![alt text](image.png)
-![alt text](image-4.png)
 ![alt text](image-2.png)
