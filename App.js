027c07c5c0b76817671b0e0a2a77e9fea78c1937
edo_eja <duyanhsadg@gmail.com>
2025-04-15 16:30:12 +0700

    s

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-// App.js
 import React from 'react';
-import AppNavigator from './src/navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import UserListScreen from './UserListScreen';
+import AddUserScreen from './AddUserScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="UserList">
+        <Stack.Screen name="UserList" component={UserListScreen} options={{ title: 'Danh sách người dùng' }} />
+        <Stack.Screen name="AddUser" component={AddUserScreen} options={{ title: 'Thêm người dùng' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
